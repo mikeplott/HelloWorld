deae88b12d2797df89f8f4b9d86f5bcebe6cbeba
michaelplott <mike.plott80@gmail.com>
2016-09-15 11:20:09 -0400

    ArrayLists and HashMaps.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by michaelplott on 9/15/16.
  */
 public class Main {
     public static void main(String[] args) {
-        String[] names = new String[3];
+    /*    String[] names = new String[3];
         names[0] = "Alice";
         names[1] = "Bob";
         names[2] = "Charlie";
@@ -22,6 +25,49 @@ public class Main {
             for(String name : names) {
                 System.out.println(name);
             }
+
+            // unboxed -> boxed
+            // int -> Integer
+            // long -> Long
+            // boolean -> Boolean
+
+            String indexStr = String.valueOf(index);
+            int indexInt = Integer.valueOf(indexStr);
+
+            Integer score = null;
+*/
+        /*
+            static | sequential | Associative
+           ----------------------------------
+           dynamic | ArrayList  | HashMap
+           ----------------------------------
+
+         */
+
+        ArrayList<String> names2 = new ArrayList();
+        names2.add("Alice");
+        names2.add("Bob");
+        names2.add("Charlie");
+       // System.out.println(names2.get(0));
+        names2.remove(1);
+        System.out.println(names2);
+
+        HashMap<String, Object> person = new HashMap();
+        person.put("name", "Alice");
+        person.put("age", 30);
+        person.put("location", "Charleston");
+        person.put("name", "Bob");
+       // System.out.println(person.remove("age"));
+        System.out.println(person);
+
+       // names2.add(person);
+        //System.out.println(names2);
+
+       // person.put("names", names2);
+        //System.out.println(person);
+        String alice = names2.get(0);
+        String bob = (String) person.get("name");
+
         }
     }
 
