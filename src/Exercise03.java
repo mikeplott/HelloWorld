a811e1dd9a3db91382bec44fc4f3e936cf730940
michaelplott <mike.plott80@gmail.com>
2016-09-21 10:28:05 -0400

    Went over other ways to loop over arrays and lists using for loops and iterators.

---
+++ src/Exercise03.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by michaelplott on 9/21/16.
+ */
+public class Exercise03 {
+    public static void main(String[] args) {
+        String[] names = {"alice", "bob", "charlie", "daniel", "andrew", "alex"};
+        // [] ["bob", "charlie", "daniel"]
+        ArrayList<String> namesArr = new ArrayList<>(Arrays.asList(names));
+        for (String name : names) {
+            if (name.startsWith("a")) {
+                namesArr.remove(name);
+            }
+        }
+        System.out.println(namesArr);
+        System.out.println();
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].startsWith("a")) {
+                namesArr.remove(names[i]);
+            }
+        }
+        System.out.println(namesArr);
+        System.out.println();
+
+        // iterator
+        namesArr = new ArrayList<>(Arrays.asList(names));
+        Iterator<String> iter = namesArr.iterator();
+        while (iter.hasNext()) {
+            String name = iter.next();
+            if (name.startsWith("a")) {
+                iter.remove();
+            }
+        }
+
+    }
+}
+
+
