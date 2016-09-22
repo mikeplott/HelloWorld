d3f52e3c8558e68f400cfa56e2173fed23c5e190
michaelplott <mike.plott80@gmail.com>
2016-09-22 09:55:11 -0400

    Added a wrapper class for multiple objects

--- src/ReadWriteJson.java
+++ src/ReadWriteJson.java
@@ -6,18 +6,24 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by michaelplott on 9/21/16.
  */
 public class ReadWriteJson {
     public static void main(String[] args) throws IOException {
-        Person p = new Person("Mike Plott", 28, true);
-        File f = new File("person.json");
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Mike Plott", 28, true));
+        people.add(new Person("Mikey Mike", 90, true));
+        people.add(new Person("Sam Jackson", 50, true));
+        PeopleWrapper pw = new PeopleWrapper(people);
+
+        File f = new File("people.json");
 
         // write json
         JsonSerializer serializer = new JsonSerializer();
-        String json = serializer.serialize(p);
+        String json = serializer.deep(true).serialize(pw);
         System.out.println(json);
         FileWriter fw = new FileWriter(f);
         fw.write(json);
@@ -29,8 +35,8 @@ public class ReadWriteJson {
         char[] contents = new char[fileSize];
         fr.read(contents, 0, fileSize);
         JsonParser parser = new JsonParser();
-        Person p2 = parser.parse(contents, Person.class);
-        System.out.println(p2);
+        PeopleWrapper pw2 = parser.parse(contents, PeopleWrapper.class);
+        System.out.println(pw2);
 //        String.valueOf(contents);
 //        System.out.println();
 //        System.out.println(contents);
